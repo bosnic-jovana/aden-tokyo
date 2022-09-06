0f7ddd5ef7219c5cddcd5706f92fb1cbc5a86bfd
bosnic-jovana <106922195+bosnic-jovana@users.noreply.github.com>
2022-09-06 19:28:14 +0200

    Delete crud directory

--- crud/api/users.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once("common/controller-includes.php");
-
-    require_once("../data/user.php");
-    $className = "User";
-
-    require_once("common/crud.php");
--- crud/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define("SERVER", "localhost");
-define("DATABASE", 	"crud");
-define("USERNAME", "root");
-define("PASSWORD", "");
